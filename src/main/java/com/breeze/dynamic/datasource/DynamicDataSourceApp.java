3845819d1c931ad5fc17fbdc80d01463329f8aba
breeze <nanhuaijin77@gmail.com>
2020-06-07 10:07:07 +0800

    feat：项目init

---
+++ src/main/java/com/breeze/dynamic/datasource/DynamicDataSourceApp.java
@@ -0,0 +1,16 @@
+package com.breeze.dynamic.datasource;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author : breeze
+ * @date : 2020/6/7
+ * @description :
+ */
+@SpringBootApplication
+public class DynamicDataSourceApp {
+    public static void main(String[] args) {
+        SpringApplication.run(DynamicDataSourceApp.class, args);
+    }
+}
